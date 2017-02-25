c4dc76be820e22ef1bc4aa964c917890bd7d4e0e
TheMastersFTC <chloejosien@gmail.com>
2017-02-25 16:16:32 -0700

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,70 @@
-k
+package cs2420;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by chloe on 2/25/2017.
+ */
+public class BinarySearchTree implements SortedSet{
+    @Override
+    public boolean add(Comparable item) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection items) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Comparable item) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection items) {
+        return false;
+    }
+
+    @Override
+    public Comparable first() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Comparable last() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Comparable item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection items) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList toArrayList() {
+        return null;
+    }
+}
